6b9c08b3314cd8e817ebfd1299a5cd5fce86262e
momo <momo@momo-desktop.(none)>
2012-06-13 00:08:49 +0800

    BabyRoad Initial Repository

---
+++ src/com/since1985i/babyroad/HomePage.java
@@ -0,0 +1,13 @@
+package com.since1985i.babyroad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomePage extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
